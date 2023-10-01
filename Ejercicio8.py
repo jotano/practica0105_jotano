4357a4be2ee710afeb9557c7dd211a8833d0c360
jotano <jotanosanm@educacion.navarra.es>
2023-10-01 15:08:37 +0200

    Ejercicio8

---
+++ Ejercicio8.py
@@ -0,0 +1,8 @@
+n = int(input("Escribe un número entero (n): "))
+m = int(input("Escribe otro número entero (m): "))
+ # <c> = cociente
+cociente = n // m
+# <r> = resto
+resto = n % m
+print(str(n) + " entre " + str(m) + " da un cociente " + str(cociente) + " y un resto " + str(resto))
+input ("Pulsa enter para salir")
