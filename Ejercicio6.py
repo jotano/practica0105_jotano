40a318082d8645968595912ffb76e3eb815ab1d5
jotano <jotanosanm@educacion.navarra.es>
2023-10-01 14:40:32 +0200

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,6 +1,6 @@
 n = int(input("Introduce un número positivo: "))
 if n <= 0:
-    print("El número que acabas de instroducir no es positivo")
+    print("El número que acabas de introducir no es positivo")
 else:
     suma = (n * (n + 1)) // 2
     print("La suma de los enteros desde 1 hasta", n, "es:", suma)
